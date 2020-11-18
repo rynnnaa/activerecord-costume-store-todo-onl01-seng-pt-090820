47555a68b4b3a2be0ecbbe85b41c679da9f027b2
Ryna Truong <rynatruong@outlook.com>
2020-11-18 19:32:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,11 @@
 # Create your costume_stores migration 
 class CostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |c|
+      c.string :name
+      c.integer :price
+      c.string :size
+      c.string :image_url
+      c.string :created_at
+      c.string :updated_at
   
