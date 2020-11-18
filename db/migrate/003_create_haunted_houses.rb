485d17db63bb49ebc0c9e84b10c195bcf503c7ef
Ryna Truong <rynatruong@outlook.com>
2020-11-18 19:36:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,11 +1,15 @@
-# Create your haunted_houses migration here
-class CreateHauntedHouses < ActiveRecord::Migration[5.1]
-  
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :haunted_houses do |c|
-      c.string :name
-      c.integer :price
-      c.string :size
-      c.string :image_url
-      c.string :created_at
-      c.string :updated_at
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+  end
+
+end
