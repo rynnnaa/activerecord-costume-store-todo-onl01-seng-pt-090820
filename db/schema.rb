a9a7006526804826a68a33635d6bd0048f5041b8
Ryna Truong <rynatruong@outlook.com>
2020-11-18 19:42:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "costumes", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.string "size"
-    t.string "image_url"
-    t.string "created_at"
-    t.string "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
