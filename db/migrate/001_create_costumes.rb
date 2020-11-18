7156d70844257108e81ecf6ad572151412ec5572
Ryna Truong <rynatruong@outlook.com>
2020-11-18 19:41:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |c|
       c.string :name
-      c.integer :price
+      c.float :price
       c.string :size
       c.string :image_url
       c.timestamps
